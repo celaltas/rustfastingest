d23ffee564bc9c241af4bc19c9f7507633366d72
celaltas <60509470+celaltas@users.noreply.github.com>
2024-04-17 00:46:04 +0300

    [ADD] Insert node added.

--- src/db/syclladb.rs
+++ src/db/syclladb.rs
@@ -1,11 +1,10 @@
 use eyre::{eyre, Result};
-use scylla::{
-    frame::{response::result::Prepared, Compression},
-    prepared_statement::PreparedStatement,
-    Session, SessionBuilder,
-};
+use scylla::{frame::Compression, prepared_statement::PreparedStatement, Session, SessionBuilder};
 use std::{collections::HashMap, fs, path::Path, sync::Arc, time::Duration};
-use tracing::info;
+use tokio::sync::Mutex;
+use tracing::{error, info};
+
+use super::model::NodeModel;
 
 const INSERT_NODE_QUERY: &str = "INSERT INTO graph.nodes (id, direction, relation, relates_to, name, ingestion_id, url, item_type, tags) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 const GET_NODE_BY_ID_QUERY: &str = "SELECT id, name, item_type, url, ingestion_id FROM graph.nodes WHERE id = ? AND direction = '' AND relation = ''";
@@ -15,12 +14,13 @@ const GET_NODE_BY_ID_AND_DIRECTION_QUERY: &str = "SELECT id, direction, relation
 const GET_NODE_BY_ID_DIRECTION_AND_RELATION_QUERY: &str = "SELECT id, direction, relation, relates_to, name, item_type FROM graph.nodes WHERE id = ? AND direction IN ('', ?) AND relation IN ('', ?)";
 
 pub struct ScyllaService {
-    client: Arc<Session>,
-    prepared_statements: Arc<HashMap<String, PreparedStatement>>,
+    client: Session,
+    prepared_statements: HashMap<String, PreparedStatement>,
+    concurrency_limit: usize,
 }
 
 impl ScyllaService {
-    pub async fn new_session(db_url: String) -> Result<Session> {
+    async fn new_session(db_url: String) -> Result<Session> {
         let session: Session = SessionBuilder::new()
             .known_node(db_url)
             .compression(Some(Compression::Lz4))
@@ -28,7 +28,11 @@ impl ScyllaService {
             .await?;
         Ok(session)
     }
-    pub async fn init<P: AsRef<Path>>(db_url: String, schema_file: P) -> Result<ScyllaService> {
+    pub async fn init<P: AsRef<Path>>(
+        db_url: String,
+        schema_file: P,
+        concurrency_limit: usize,
+    ) -> Result<ScyllaService> {
         let session = ScyllaService::new_session(db_url).await?;
         info!("Scylla service: Connection established.");
         ScyllaService::load_schema(&session, schema_file).await?;
@@ -36,8 +40,9 @@ impl ScyllaService {
         let prepared_statements = ScyllaService::create_prepared_statements(&session).await?;
         info!("Scylla service: All prepared statements are created.");
         Ok(ScyllaService {
-            client: Arc::new(session),
-            prepared_statements: Arc::new(prepared_statements),
+            client: session,
+            prepared_statements,
+            concurrency_limit,
         })
     }
 
@@ -58,7 +63,6 @@ impl ScyllaService {
     async fn run_queries(session: &Session, queries: Vec<String>) -> Result<()> {
         for query in queries {
             info!("Running Query {}", query);
-            println!("Running Query {}", query);
             session.query(query, &[]).await?;
         }
         Ok(())
@@ -103,6 +107,53 @@ impl ScyllaService {
 
         Ok(prepared_statements)
     }
+
+    pub async fn insert_nodes(service: ScyllaService, nodes: Vec<NodeModel>) -> Result<()> {
+        let service = Arc::new(Mutex::new(service));
+        let mut handles = vec![];
+        for node in nodes {
+            let service = Arc::clone(&service);
+            let handle = tokio::spawn(async move {
+                let service = service.lock().await;
+                service.insert_node(node).await
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            let res = handle.await?;
+            match res {
+                Ok(_) => info!("The record save successfully"),
+                Err(_) => error!("Error occured when record saved"),
+            }
+        }
+
+        Ok(())
+    }
+    async fn insert_node(&self, node: NodeModel) -> Result<()> {
+        let ps = self
+            .prepared_statements
+            .get("insert_node_query")
+            .ok_or_else(|| eyre!("insert node prepared statement not found"))?;
+        let _res = self
+            .client
+            .execute(
+                &ps,
+                (
+                    node.uuid,
+                    node.direction,
+                    node.relation,
+                    node.relates_to,
+                    node.name,
+                    node.ingestion_id,
+                    node.url,
+                    node.node_type,
+                    node.tags,
+                ),
+            )
+            .await?;
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -110,6 +161,138 @@ mod tests {
     use super::*;
     use std::io::Write;
     use tempfile::NamedTempFile;
+    use uuid::Uuid;
+
+    #[tokio::test]
+    async fn test_insert_nodes() {
+        let mut nodes: Vec<NodeModel> = Vec::new();
+        for i in 0..10 {
+            let uuid = Uuid::new_v4();
+            let direction = if i % 2 == 0 {
+                Some("Outgoing".to_string())
+            } else {
+                Some("Incoming".to_string())
+            };
+            let relation = Some(format!("relation_{}", i));
+            let relates_to = Some(format!("relates_to_{}", i));
+            let name = format!("Node {}", i);
+            let ingestion_id = format!("ID {}", i);
+            let url = format!("https://example.com/node/{}", i);
+            let node_type = if i % 3 == 0 {
+                "Type A".to_string()
+            } else {
+                "Type B".to_string()
+            };
+            let tags = Some(vec![
+                ("tag1".to_string(), format!("value{}", i)),
+                ("tag2".to_string(), format!("value{}", i * 2)),
+            ]);
+            let node = NodeModel {
+                uuid,
+                direction,
+                relation,
+                relates_to,
+                name,
+                ingestion_id,
+                url,
+                node_type,
+                tags,
+            };
+            nodes.push(node);
+        }
+
+        let db_url = "127.0.0.1:9042".to_string();
+        let mut schema_file = std::env::current_dir().expect("failed get current dir");
+        schema_file.push("schema/ddl.cql");
+        let service = ScyllaService::init(db_url, schema_file, 1)
+            .await
+            .expect("Initialization database failed:");
+
+        let result = ScyllaService::insert_nodes(service, nodes).await;
+        assert!(result.is_ok());
+
+        let session = ScyllaService::new_session("127.0.0.1:9042".to_string())
+            .await
+            .expect("failed connection to scylladb");
+        let res = session
+            .query("SELECT * FROM graph.nodes", &[])
+            .await
+            .expect("Failed to query database");
+        if let Some(rows) = res.rows {
+            assert_eq!(10, rows.len());
+        }
+
+        session
+            .query("DROP TABLE IF EXISTS graph.nodes;", &[])
+            .await
+            .expect("Failed to drop table");
+        session
+            .query("DROP KEYSPACE IF EXISTS graph;", &[])
+            .await
+            .expect("Failed to drop keyspace");
+    }
+
+    #[tokio::test]
+    async fn test_insert_node() {
+        let uuid = Uuid::new_v4();
+        let direction = Some("Outgoing".to_string());
+        let relation = Some("parent".to_string());
+        let relates_to = Some("123456".to_string());
+        let name = "Example Node".to_string();
+        let ingestion_id = "ABC123".to_string();
+        let url = "https://example.com".to_string();
+        let node_type = "Page".to_string();
+        let tags = Some(vec![
+            ("tag1".to_string(), "value1".to_string()),
+            ("tag2".to_string(), "value2".to_string()),
+        ]);
+
+        let node = NodeModel {
+            uuid,
+            direction,
+            relation,
+            relates_to,
+            name,
+            ingestion_id,
+            url,
+            node_type,
+            tags,
+        };
+        let db_url = "127.0.0.1:9042".to_string();
+        let mut schema_file = std::env::current_dir().expect("failed get current dir");
+        schema_file.push("schema/ddl.cql");
+        let service = ScyllaService::init(db_url, schema_file, 1)
+            .await
+            .expect("Initialization database failed:");
+        service
+            .insert_node(node)
+            .await
+            .expect("Insert node failed:");
+
+        let res = service
+            .client
+            .query("SELECT name FROM graph.nodes LIMIT 1", &[])
+            .await
+            .expect("Failed to query database");
+        if let Some(rows) = res.rows {
+            for row in rows {
+                let (name,): (String,) = row
+                    .into_typed::<(String,)>()
+                    .expect("parsing error query result");
+                assert_eq!(name, "Example Node".to_string());
+            }
+        }
+        service
+            .client
+            .query("DROP TABLE IF EXISTS graph.nodes;", &[])
+            .await
+            .expect("Failed to drop table");
+        service
+            .client
+            .query("DROP KEYSPACE IF EXISTS graph;", &[])
+            .await
+            .expect("Failed to drop keyspace");
+    }
 
     #[tokio::test]
     async fn test_init() {
@@ -130,7 +313,7 @@ mod tests {
         schema_file
             .write_all(cql.as_bytes())
             .expect("failed write to file");
-        let service = ScyllaService::init(db_url, schema_file.path())
+        let service = ScyllaService::init(db_url, schema_file.path(), 1)
             .await
             .expect("Initialization database failed:");
         let res = service
